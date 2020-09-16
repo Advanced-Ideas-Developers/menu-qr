41a449b30010b71974fc2857e61d19c1e49138a0
Hamilton Garcia <hngr98@gmail.com>
2020-09-15 23:17:17 -0600

    URLS

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import LasTejitasView
+from .views import LasTejitasView, HongKongView, TacontentoView
 
 urlpatterns = [
-  path('las-tejitas/', LasTejitasView.as_view(), name='las-tejitas')
+  path('las-tejitas/', LasTejitasView.as_view(), name='las-tejitas'),
+  path('hong-kong/', HongKongView.as_view(), name='hong-kong'),
+  path('tacontento/', TacontentoView.as_view(), name='tacontento'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 class LasTejitasView(TemplateView):
     template_name = 'restaurants/las_tejitas.html'
+
+class HongKongView(TemplateView):
+    template_name = 'restaurants/hong_kong.html'
+
+class TacontentoView(TemplateView):
+    template_name = 'restaurants/tacontento.html'
