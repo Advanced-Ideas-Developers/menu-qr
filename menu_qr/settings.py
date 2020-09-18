6b01d4bcc653ecf92822de18b02d2b28481c1704
RaveHunter05 <paulsotelo97@gmail.com>
2020-09-17 22:46:02 -0600

    tratando de agregar el mail

--- menu_qr/settings.py
+++ menu_qr/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'paulsotelo97@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = ''
