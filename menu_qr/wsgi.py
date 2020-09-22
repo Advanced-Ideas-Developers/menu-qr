d85058d7458f314d15b290d1af9fbf65d1d9e913
RaveHunter05 <paulsotelo97@gmail.com>
2020-09-22 09:49:15 -0600

    pruebas para deploy

--- menu_qr/wsgi.py
+++ menu_qr/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dj_static import Cling, MediaCling
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menu_qr.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
