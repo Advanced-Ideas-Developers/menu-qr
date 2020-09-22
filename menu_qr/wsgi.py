ccd6d6619818482e3b54dca13d244e7fdcb15e6e
RaveHunter05 <paulsotelo97@gmail.com>
2020-09-22 10:23:11 -0600

    cling

--- menu_qr/wsgi.py
+++ menu_qr/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'menu_qr.settings')
 
