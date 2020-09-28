e52f88db1fbad1261779ab28473285ba96b602aa
Hamilton Garcia <hngr98@gmail.com>
2020-09-28 13:01:12 -0600

    some changes

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .views import LasTejitasView, HongKongView, TacontentoView, successView, menu
 
 urlpatterns = [
-  path('las-tejitas/', LasTejitasView.as_view(), name='las-tejitas'),
-  path('hong-kong/', HongKongView.as_view(), name='hong-kong'),
-  path('tacontento/', TacontentoView.as_view(), name='tacontento'),
-  path('success/', successView, name='success'),
-  path('menu/<link>', menu)
+    path('las-tejitas/', LasTejitasView.as_view(), name='las-tejitas'),
+    path('hong-kong/', HongKongView.as_view(), name='hong-kong'),
+    path('tacontento/', TacontentoView.as_view(), name='tacontento'),
+    path('success/', successView, name='success'),
+    path('menu/<link>', menu)
 ]
