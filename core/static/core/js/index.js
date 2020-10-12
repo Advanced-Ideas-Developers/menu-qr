179cee4daa68ddcb7acc8f7b3a29138ea2903660
Hamilton Garcia <hngr98@gmail.com>
2020-10-12 15:34:13 -0600

    cambios contactos

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -1,8 +1,8 @@
 $('.modal').delay(3000).fadeOut();
 
-$('.plan').hover(function(){
+$('.plan').hover(function () {
   $(this).find('.btn').slideDown();
-},function(){
+}, function () {
   $(this).find('.btn').slideUp();
 });
 
@@ -10,59 +10,76 @@ $(function () {
   $(document).scroll(function () {
     var $nav = $("nav");
     $('.menu-resp').toggleClass('scrolled', $(this).scrollTop() > $('.menu-resp').height());
-    if(window.screen.width>768){
+    if (window.screen.width > 768) {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-      if($(this).scrollTop() > $nav.height()){
+      if ($(this).scrollTop() > $nav.height()) {
         $('#logoAID').show(200);
-      }else{
+      } else {
         $('#logoAID').hide();
       }
     }
   });
 });
 
-function goToByScroll(id){
+function goToByScroll(id) {
   id = id.replace("link", "");
-  if(id === 'inicio'){
+  if (id === 'inicio') {
     $('html,body').animate({
-      scrollTop: 0},
+      scrollTop: 0
+    },
       'slow');
-    if(window.screen.width < 728){
+    if (window.screen.width < 728) {
       $('nav').toggleClass('expanded');
     }
-  }else{
+  } else {
     $('html,body').animate({
-      scrollTop: $("#"+id).offset().top - 100},
+      scrollTop: $("#" + id).offset().top - 100
+    },
       'slow');
-      if(window.screen.width < 728){
-        $('nav').toggleClass('expanded');
-      }
+    if (window.screen.width < 728) {
+      $('nav').toggleClass('expanded');
+    }
   }
 }
 
-$(".nav-item").click(function(e) { 
-  e.preventDefault(); 
-  goToByScroll($(this).attr("id"));           
+function goToByScroll2(id) {
+  id = id.replace("link", "");
+  if (id === 'inicio') {
+    $('html,body').animate({
+      scrollTop: 0
+    },
+      'slow');
+  } else {
+    $('html,body').animate({
+      scrollTop: $("#" + id).offset().top - 100
+    },
+      'slow');
+  }
+}
+
+$(".nav-item").click(function (e) {
+  e.preventDefault();
+  goToByScroll($(this).attr("id"));
 });
 
-$(".fa-bars").click(function(e) {
+$(".fa-bars").click(function (e) {
   //$("nav").slideDown(200);
   $('nav').toggleClass('expanded');
 });
 
-$(".fa-times").click(function() {
+$(".fa-times").click(function () {
   //$("nav").slideUp(200);
   $('nav').toggleClass('expanded');
 })
 
-$('.contact').click(function() {
-  goToByScroll("contactlink");
+$('.contact').click(function () {
+  goToByScroll2("contactlink");
 });
 
-$('.contactar').click(function() {
-  goToByScroll("contactlink");
+$('.contactar').click(function () {
+  goToByScroll2("contactlink");
 });
 
-$('.test').click(function() {
-  goToByScroll("clientslink");
+$('.test').click(function () {
+  goToByScroll2("clientslink");
 });
